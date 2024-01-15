f19966fee495b2fe86b8edc99ab4ee88fc979f6f
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-01-15 11:53:08 -0400

    feat: Criando arquivos constants

---
+++ src/modules/_shared/constants/blackParts.constans.ts
@@ -0,0 +1,18 @@
+export const blackParts = [
+  { name: 'Torre Preta', text: '♜' },
+  { name: 'Cavalo Preto', text: '♞' },
+  { name: 'Bispo Preto', text: '♝' },
+  { name: 'Rei Preto', text: '♚' },
+  { name: 'Rainha Preta', text: '♛' },
+  { name: 'Bispo Preto', text: '♝' },
+  { name: 'Cavalo Preto', text: '♞' },
+  { name: 'Torre Preta', text: '♜' },
+  { name: 'Peão Preto', text: '♟' },
+  { name: 'Peão Preto', text: '♟' },
+  { name: 'Peão Preto', text: '♟' },
+  { name: 'Peão Preto', text: '♟' },
+  { name: 'Peão Preto', text: '♟' },
+  { name: 'Peão Preto', text: '♟' },
+  { name: 'Peão Preto', text: '♟' },
+  { name: 'Peão Preto', text: '♟' }
+];
---
+++ src/modules/_shared/constants/whiteParts.constans.ts
@@ -0,0 +1,18 @@
+export const whiteParts = [
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Peão Branco', text: '♙' },
+  { name: 'Torre Branca', text: '♖' },
+  { name: 'Cavalo Branco', text: '♘' },
+  { name: 'Bispo Branco', text: '♗' },
+  { name: 'Rei Branco', text: '♔' },
+  { name: 'Rainha Branca', text: '♕' },
+  { name: 'Bispo Branco', text: '♗' },
+  { name: 'Cavalo Branco', text: '♘' },
+  { name: 'Torre Branca', text: '♖' }
+];
